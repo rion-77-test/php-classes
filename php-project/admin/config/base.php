24624edceb9cc74f706146f5531428961aa4e0a2
rion-77-test <testmailrion77@gmail.com>
2026-06-04 13:09:00 +0600

    templating introduction

---
+++ php-project/admin/config/base.php
@@ -0,0 +1,9 @@
+<?php
+//Local
+const BASE_URL = "http://localhost/project-php/";
+const BASE_URL_ADMIN = "http://localhost/php-project/admin/";
+
+//Hosting
+// const BASE_URL = "http://eduhub.rioncretes.com";
+// const BASE_URL_ADMIN = "http://eduhub.rioncretes.com/admin/";
+
